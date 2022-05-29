9e8b16e60ba329bd4a733a3430143b21dd942a59
ryanmadsenn <mad19002@byui.edu>
2022-05-28 18:34:34 -0600

    removed console.logs

--- chamber/js/wind-chill.js
+++ chamber/js/wind-chill.js
@@ -2,9 +2,6 @@ let temp = document.getElementById('temp').textContent
 let mph = document.getElementById('mph').textContent
 let windChillContainer = document.getElementById('wind-chill')
 
-console.log(temp)
-console.log(mph)
-
 if(temp <= 50 & mph > 3) {
     windChill = 35.74 + 0.6215 * temp - 35.75 * mph ** 0.16 + 0.4275 * temp * mph ** 0.16
     windChillContainer.textContent = Math.round(windChill)
